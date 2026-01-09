94f657df439f534888a9ccbeb11e9cc7c7fa581d
jeonlyung <bindongx2@naver.com>
2026-01-09 12:30:01 +0900

    공통 상수 파일 추가

---
+++ src/main/java/com/spring/framework/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.spring.framework.constant;
+
+public class Constants {
+    public final static String SESSION_VO = "DORIDORITEAM";
+
+    /* 기본 변수 */
+    public static final String SUCCESS_Y = "Y";
+    public static final String SUCCESS_N = "N";
+    public static final boolean BOOLEAN_TRUE = true;
+    public static final boolean BOOLEAN_FLASE = false;
+}
