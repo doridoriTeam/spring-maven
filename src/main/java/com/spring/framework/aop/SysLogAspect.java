699d9b8cbe10325b149ef2f03586ef85db9c3f3c
jeonlyung <bindongx2@naver.com>
2026-01-11 22:20:03 +0900

    시스템 로그 추가

--- src/main/java/com/spring/framework/aop/SysLogAspect.java
+++ src/main/java/com/spring/framework/aop/SysLogAspect.java
@@ -2,4 +2,9 @@ package com.spring.framework.aop;
 
 public class SysLogAspect {
     
+
+    public static void logTest() {
+        System.out.println("SysLogAspect logTest()");    
+    }
+    
 }
