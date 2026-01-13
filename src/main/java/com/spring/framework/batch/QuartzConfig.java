bf081024fb671a3f6f14ee7550c92fbdca73a53e
jeonlyung <bindongx2@naver.com>
2026-01-13 09:39:21 +0900

    QuartzConfig 파일 변경 및  추가

--- src/main/java/com/spring/framework/batch/QuartzConfig.java
+++ src/main/java/com/spring/framework/batch/QuartzConfig.java
@@ -3,6 +3,8 @@ package com.spring.framework.batch;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -11,12 +13,24 @@ import org.springframework.core.env.Environment;
 @PropertySource("classpath:/properties/quartz.properties")
 public class QuartzConfig {
 
-    
+    @Autowired
     private Environment env;
 
+    @Bean(name = "schedulerSetting")
     public List<QuartzDto> schedulerSetting() {
-        List<QuartzDto> items = new ArrayList<>();
-       
-        return items;
+        List<QuartzDto> list = new ArrayList<>();
+        String items = env.getProperty("job.list");
+        if (items != null) {
+            String[] itemArray = items.split(",");
+            for (String item : itemArray) {
+                String jobName = env.getProperty(item + ".jobName");
+                String jobGroup = env.getProperty(item + ".jobGroup");
+                String triggerName = env.getProperty(item + ".triggerName");
+                String triggerGroup = env.getProperty(item + ".triggerGroup");
+                
+                QuartzDto dto = new QuartzDto(jobName, jobGroup, triggerName, triggerGroup);
+                list.add(dto);
+            }
+        return list;
     }
 }
